471ce8c4fe8a42202c52b1f28864c19cb1b36ada
jkim810 <jkim0810@gmail.com>
2017-10-16 14:04:55 -0400

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-#include <string>
 #include "hash.h"
+
+using namespace std;
 
 hashTable::hashTable(int size){
 
@@ -9,22 +9,22 @@ hashTable::hashTable(int size){
 	data.assign(capacity, hashItem("",false,false,NULL));
 }
 
-int hashTable::insert(const std::string &key, void *pv){
-
+int hashTable::insert(const string &key, void *pv){
+	//rehash if half of the table is filled
 	if(capacity == 0 || filled > capacity/2){
 		if(rehash() == false){
 			return 2;
 		}
 	}
 
-	if(contains(key)){
-		std::cerr<< "Error: Key exists" <<std::endl;
-		return 1;
-	}
+	//return 1 if key exists
+	if(contains(key)) return 1;
 
+	//otherwise insert
 	hashItem *itemToInsert = new hashItem(key, true, false, NULL);
 	int position = hash(key);
 
+	//linear probing
 	while(data.at(position).isOccupied){
 		position++;
 		if(position == capacity) position = 0;
@@ -32,40 +32,34 @@ int hashTable::insert(const std::string &key, void *pv){
 
 	data.at(position) = *itemToInsert;
 	filled++;
-
 	return 0;
-	
 }
 
 // Check if the specified key is in the hash table.
 // If so, return true; otherwise, return false.
-bool hashTable::contains(const std::string &key){
+bool hashTable::contains(const string &key){
 	if(findPos(key) == -1) return false;
 	return true;
 }
 
-hashTable::hashItem::hashItem(const std::string &key, bool isOccupied, bool isDeleted, void *pv)
+hashTable::hashItem::hashItem(const string &key, bool isOccupied, bool isDeleted, void *pv)
     :key(key), isOccupied(isOccupied), isDeleted(isDeleted), pv(pv) { }
 
-//void *hashTable::getPointer(const std::string &key, bool *b){ }
-int hashTable::setPointer(const std::string &key, void *pv){ return -1; }
-bool hashTable::remove(const std::string &key){ return false; }
+//void *hashTable::getPointer(const string &key, bool *b){ }
+int hashTable::setPointer(const string &key, void *pv){ return -1;}
 
-int hashTable::hash(const std::string &key){
-	int hashVal = 0;
-	for(int i = 0; i < key.size(); i++){
-		hashVal = 37 * hashVal + key.at(i);
-	}
-	int moduloHashVal = hashVal % capacity;
-	if(moduloHashVal < 0) moduloHashVal += capacity;
-	return moduloHashVal;
+bool hashTable::remove(const string &key){ return false; }
+
+int hashTable::hash(const string &key){
+	unsigned int hash = 5381;
+	for(int i = 0; i < key.length(); i++)  hash = ((hash << 5) + hash) + key.at(i); 
+	hash = hash%capacity;
+    return (int) hash;
 }
 
-int hashTable::findPos(const std::string &key){
+int hashTable::findPos(const string &key){
 	int position = hash(key);
-	//std::cout<< "position: " << position << std::endl;
 	while(data.at(position).isOccupied == true){
-		//std::cout<<"error?"<<std::endl;
 		if(data.at(position).key.compare(key) == 0){
 			return position;
 		}
@@ -78,20 +72,20 @@ int hashTable::findPos(const std::string &key){
 bool hashTable::rehash(){
 	capacity = getPrime(filled);
 	if(capacity == 0){
-		std::cout<< "Error: Rehash Failed. Data set too big." <<std::endl;
+		cout<< "Error: Rehash Failed. Data set too big." <<endl;
 		return false;
 	}
-	std::vector<hashItem> tmp = data;
+	vector<hashItem> tmp = data;
 	data.assign(capacity,hashItem("",false,false,NULL));
 	filled = 0;
-	for(std::vector<hashItem>::iterator it = tmp.begin(); it!=tmp.end(); it++){
+	for(vector<hashItem>::iterator it = tmp.begin(); it!=tmp.end(); it++){
 		if(it->isOccupied) insert(it->key);
 	}
 	return true;
 }
 
 unsigned int hashTable::getPrime(int size){
-	static unsigned int primeList[] = {50021, 100003, 200003, 500009, 1000003, 2000003 };
+	static unsigned int primeList[] = {50021, 100003, 200003, 500009, 1000003, 2000003, 4000037 };
 	for(int i = 0; i < 6; i++){
 		if(primeList[i]/2 > size){
 			return primeList[i];
